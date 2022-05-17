48b6b65423d5390e0256cf571331bfcd31e42cdb
Alper Konuralp <alper@konuralp.gen.tr>
2022-05-18 00:48:21 +0300

    Axios Demo

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Vue logo" src="./assets/logo.png" style="height: 10%" />
+    <h1>Ülkeler:</h1>
+    <table align="center">
+      <thead>
+        <tr>
+          <th>Alpha Kod 2</th>
+          <th>Alpha Kod 3</th>
+          <th>İsim</th>
+          <th>Başkent</th>
+          <th>Bölge</th>
+          <th>Nüfus</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="country of countries" :key="country.alpha3Code">
+          <td>{{ country.alpha2Code }}</td>
+          <td>{{ country.alpha3Code }}</td>
+          <td>{{ country.name }}</td>
+          <td>{{ country.capital }}</td>
+          <td>{{ country.region }} / {{ country.subregion }}</td>
+          <td align="right">{{ country.population }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
+import { Country } from "./types";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+@Component
+export default class App extends Vue {
+  private countries: Country[] = [];
+
+  created() {
+    axios
+      .get("https://restcountries.com/v2/all")
+      .then((x) => (this.countries = x.data));
+  }
+}
 </script>
 
 <style lang="scss">
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface Country {
+    name: string;
+    alpha2Code: string;
+    alpha3Code: string;
+    capital: string;
+    subregion: string;
+    region: string;
+    population: number;
+    nativeName: string;
+    flag: string;
+}
